9381cd9cac8790f2d4d172725bf98e720f325dc6
MLisStar <90730254+MLisStar@users.noreply.github.com>
2023-03-09 21:13:57 +0300

    testing

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 100-my_calculator.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div 
     import sys
 
     if len(sys.argv) - 1 != 3:
