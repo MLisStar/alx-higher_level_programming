df97aa357e80c5d267a00e473450cb082b2a03dc
MLisStar <90730254+MLisStar@users.noreply.github.com>
2023-03-16 01:35:47 +0300

    fixing

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+# 5-number_keys.py
+
+
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-    return (num)
-
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
