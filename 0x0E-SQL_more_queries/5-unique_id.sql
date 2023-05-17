2273fdb8eae1912c56dcf7b60faa6c95b2dfdfff
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-18 01:24:26 +0300

    FIX: fix to unique_id and states

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create table unique_id
-CREATE TABLE IN NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
