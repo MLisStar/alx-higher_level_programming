39276846bf4ce46907680f5bc519db4660a82b4b
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-19 01:45:25 +0300

    FIX

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,6 @@
---List the scores and the number of times they appear in the second_table
+-- List the scores and the number of times they appear in the second_table
 -- Lists the number of records with the same score
-
-SELECT score, COUNT(*) AS number
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
