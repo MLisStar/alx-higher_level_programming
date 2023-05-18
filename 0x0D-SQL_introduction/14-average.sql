7d2788fe1a98f0089a6d868549844509533d2092
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-19 00:26:34 +0300

    FIX

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,6 @@
---This script should get the average of score from ths table.
---Then assign the calculated value to the column "average"
-SELECT AVG(score) as average FROM second_table;
+-- Computes the `score` average of all records in
+-- the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+
+SELECT AVG(`score`) as `average`
+FROM second_table;
