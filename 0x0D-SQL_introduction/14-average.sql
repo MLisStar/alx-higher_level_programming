f7d52c8b4ffa6be4ca04d59c3dad8159c160afa2
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-19 00:25:37 +0300

    FIX: change to AS

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 --This script should get the average of score from ths table.
 --Then assign the calculated value to the column "average"
-SELECT AVG(score) AS average FROM second_table;
+SELECT AVG(score) as average FROM second_table;
