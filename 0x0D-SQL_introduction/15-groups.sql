9c8c79bd8f0739cb76b06c2fa726d3bf6c66f85f
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-19 01:15:23 +0300

    FIX: remove quotes on number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 --List the scores and the number of times they appear in the second_table
 -- Lists the number of records with the same score
 
-SELECT score, COUNT(*) as 'number'
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY number DESC;
