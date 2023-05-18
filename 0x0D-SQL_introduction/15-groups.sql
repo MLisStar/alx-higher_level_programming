a5cf332a55553ee38bf74ef4149336b4d6a8706e
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-19 00:51:50 +0300

    FEATURE: using ORDER BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,7 @@
---List the scores and the number of times they appear in the second table.
-SELECT score, COUNT(score) as number
-FROM second_table GROUP BY score
+--List the scores and the number of times they appear in the second_table
+-- Lists the number of records with the same score
+
+SELECT score, COUNT(`score`) as number
+FROM second_table
+GROUP BY score
 ORDER BY score DESC;
