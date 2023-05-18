310fa4a613350a88d62fcbdf38114a72d46c5ce1
MLisStar <m3lissa.ihiga@gmail.com>
2023-05-18 23:39:32 +0300

    FEATURE: list records, name and score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- This script displays all records with score>=10 in second table.
 -- Only the score and the name solumns should be displayed.
-SELECT score, name FROM second_table WHERE score>=10;
+SELECT score, name FROM second_table WHERE score>=10
 ORDER BY score DESC;
