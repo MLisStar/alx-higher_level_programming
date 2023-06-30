6924510c64d702521ebbae78b5b02e01cf544cfd
MLisStar <m3lissa.ihiga@gmail.com>
2023-07-01 00:12:38 +0300

    test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,21 @@
 """Defines a peak-finding algorithm."""
 
 
-def find_peak(list_of_integers):
+def find_peak(list_of_integers, start=None, end=None):
     """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(list_of_integers) - 1
+
+    if start > end:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    mid = int(size / 2)
+    mid = start + (end - start) // 2
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
     elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+        return find_peak(list_of_integers, start, mid - 1)
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return find_peak(list_of_integers, mid + 1, end)
